6b7ecbed37afe3975b2afcd83be853fe58817f15
Vinnie117 <hhang@hotmail.de>
2022-07-24 00:55:23 +0200

    comments on matplotlib backend

--- test.py
+++ test.py
@@ -1,37 +1,39 @@
 from openbb_terminal import api as openbb
 import matplotlib.pyplot as plt 
+import matplotlib
+import matplotlib.rcsetup as rcsetup
+
+# https://stackoverflow.com/questions/7534453/matplotlib-does-not-show-my-drawings-although-i-call-pyplot-show
+print(matplotlib.matplotlib_fname())
+print(rcsetup.all_backends)
+#matplotlib.rcParams['interactive'] == True
+
+# https://stackoverflow.com/questions/21835847/matplotlib-show-method-does-not-open-window
+# returns 'agg' -> non-interactive backend -> can only write to files!!
+print(plt.get_backend())
+# matplotlib.use('Agg')
+
+# To save plots using the non-interactive backends, use the matplotlib.pyplot.savefig('filename') method.
 
 print("START")
 
 aapl_data = openbb.stocks.load(
     ticker="aapl",
 )
-aapl_data = openbb.stocks.process_candle(aapl_data)
 
 
-openbb.stocks.candle("AAPL", aapl_data, True)
+candle = openbb.stocks.process_candle(aapl_data)
 
+# The data is in here
+print(type(aapl_data))
+print(aapl_data)
 
-# # Load stock data
-# ticker = "AAPL"
-# stock_data = openbb.stocks.load(ticker="aapl")
+# This is supposed to print a plot
+openbb.stocks.candle("AAPL", candle, True)
 
-# # Create a matplotlib figure
-# fig, axes = plt.subplots(3, 1, figsize=(10, 7))
-
-# # Use GST to add charts to the axes of the created figure
-# openbb.stocks.ta.macd(s_ticker=ticker, series=aapl["Close"], external_axes=axes[0:2])
-# openbb.stocks.options.voi_yf(
-#     ticker=ticker,
-#     expiry="2022-03-11",
-#     min_sp=110,
-#     max_sp=210,
-#     min_vol=0.3,
-#     external_axes=[axes[2]],
-# )
-
-# # Show the figure with charts
-# fig.show()
+# An own custom plot
+plt.plot(aapl_data.index.values, aapl_data['Close'])
+plt.show()
 
 
 print("END")
