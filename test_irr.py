d284ce7143ddcc53fd9ae64214bffc11836686ee
Vinnie117 <hhang@hotmail.de>
2022-08-04 21:21:07 +0200

    Zahlenformat bearbeitet

--- test_irr.py
+++ test_irr.py
@@ -1,8 +1,22 @@
-import numpy as np
+import pandas as pd
 import numpy_financial as npf
 
+
+df_wertpapierkonto = pd.read_csv('data\wertpapierkonto.csv', header=0, sep=';', decimal=',')
+print(df_wertpapierkonto.dtypes)
+
+# Zahlenformat
+df_wertpapierkonto['Wert'] = [x.replace('.', '') for x in df_wertpapierkonto['Wert']]
+df_wertpapierkonto['Wert'] = [x.replace(',', '.') for x in df_wertpapierkonto['Wert']]
+df_wertpapierkonto['Wert'] = df_wertpapierkonto['Wert'].astype(float)
+print(df_wertpapierkonto.dtypes)
+
+# Muss 'Wert' richtig formatieren! Als Zahl und Dezimalzeichen!
+divs = df_wertpapierkonto.loc[df_wertpapierkonto['Typ'] == 'Dividende', 'Wert'].sum()
+print(divs)
+
 # 9930 ist Einkaufssumme aller WPs
-flows = [-9930, 2946.83, 1330.25, 1408.43, 2122.72, 1635.28, 486.4]
+flows = [-9930, divs]
 
 irr = npf.irr(flows)
 print(irr)
