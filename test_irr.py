49b648a447f5d20e60fd8a63979da8637e890625
Vinnie117 <hhang@hotmail.de>
2022-08-04 22:22:56 +0200

    IRR aus PP gefunden

--- test_irr.py
+++ test_irr.py
@@ -23,9 +23,20 @@ print(divs)
 # https://www.youtube.com/watch?v=3L4T-UM3iqs
 dates = [date(2019, 1, 1), date(2019, 7, 1), date(2020, 1, 1)]
 amounts = [-50, -50, 105]
-
 irr = xirr(dates, amounts)
 print(irr)
+
+dates2 = [date(2019, 1, 7), date(2019, 1, 7), date(2020, 1, 7), date(2019, 1, 7), date(2019, 1, 7), date(2020, 1, 7),
+          date(2022, 8, 4)]
+amounts2 = [-2946.82, -1330.25, -2122.72, -1635.28, -486.4, -1408.4, 7647.85]
+irr2 = xirr(dates2, amounts2)
+print(irr2)
+
+# IRR aus PP Dashboard! (Divs werden nicht reinvestiert)
+dates3 = [date(2019, 8, 4), date(2022, 8, 4)]
+amounts3 = [-11325.6, 13651.88]
+irr3 = xirr(dates3, amounts3)
+print(irr3)
 
 
 print('END')
